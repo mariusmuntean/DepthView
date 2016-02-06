bd203c1dfccd30fd9ec92496f4d35eb63f7a031c
Marius <marius.muntean@live.com>
2016-02-06 16:46:05 +0100

    Using a CustomMvxFileDownloadCache implementation that caches the downloaded file including their original file extension

--- DepthViewer/Setup.cs
+++ DepthViewer/Setup.cs
@@ -1,14 +1,18 @@
 using Android.Content;
-using Android.Views;
+using Android.Graphics;
 using Cirrious.CrossCore;
 using Cirrious.CrossCore.Platform;
-using Cirrious.CrossCore.Plugins;
 using Cirrious.MvvmCross.Droid.Platform;
 using Cirrious.MvvmCross.Plugins.DownloadCache;
 using Cirrious.MvvmCross.Plugins.DownloadCache.Droid;
 using Cirrious.MvvmCross.ViewModels;
 using DepthViewer.Contracts;
 using DepthViewer.Services;
+using DepthViewer.Utils;
+using UniversalImageLoader.Cache.Memory;
+using UniversalImageLoader.Core;
+using UniversalImageLoader.Core.Assist;
+using UniversalImageLoader.Utils;
 
 namespace DepthViewer
 {
@@ -16,6 +20,22 @@ namespace DepthViewer
     {
         public Setup(Context applicationContext) : base(applicationContext)
         {
+
+            // Set up UniversalImageLoader
+            var options = new DisplayImageOptions.Builder()
+                .CacheInMemory(true)
+                .CacheOnDisk(false)
+                .ConsiderExifParams(true)
+                .ShowImageForEmptyUri(Resource.Drawable.splash)
+                .ImageScaleType(ImageScaleType.InSampleInt)
+                .BitmapConfig(Bitmap.Config.Rgb565)
+                .ResetViewBeforeLoading(true)
+                .Build();
+            var uilConfig = new ImageLoaderConfiguration.Builder(applicationContext)
+                .DefaultDisplayImageOptions(options)
+                .Build();
+            ImageLoader.Instance.Init(uilConfig);
+
         }
 
         protected override IMvxApplication CreateApp()
@@ -43,10 +63,9 @@ namespace DepthViewer
 
             Cirrious.MvvmCross.Plugins.File.PluginLoader.Instance.EnsureLoaded();
             Cirrious.MvvmCross.Plugins.Json.PluginLoader.Instance.EnsureLoaded();
-            Cirrious.MvvmCross.Plugins.DownloadCache.PluginLoader.Instance.EnsureLoaded();
 
             var downdloadCacheConfig = new MvxDownloadCacheConfiguration();
-            var fileDownloadCache = new MvxFileDownloadCache(downdloadCacheConfig.CacheName, downdloadCacheConfig.CacheFolderPath, downdloadCacheConfig.MaxFiles, downdloadCacheConfig.MaxFileAge);
+            var fileDownloadCache = new CustomMvxFileDownloadCache(downdloadCacheConfig.CacheName, downdloadCacheConfig.CacheFolderPath, downdloadCacheConfig.MaxFiles, downdloadCacheConfig.MaxFileAge);
 
             Mvx.LazyConstructAndRegisterSingleton<IMvxFileDownloadCache>(() => fileDownloadCache);
         }
