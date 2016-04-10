8828c5c05396bea1c83c85b603b536e8bca0af80
Marius <marius.muntean@live.com>
2016-04-10 14:49:15 +0200

    Added the number of measurements in a mapping to the teaser template

---
+++ DepthViewer/Views/ValToHumanReadableStringConverter.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Platform.Converters;
+
+namespace DepthViewer.Views
+{
+    public class ValToHumanReadableStringConverter:MvxValueConverter<int, string>
+    {
+        protected override string Convert(int value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == 1? "Only one measurement": $"{value} measurements";
+        }
+    }
+}
